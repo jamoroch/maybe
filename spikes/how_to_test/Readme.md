af752c62a76b58befca8a3af5810267d2431afd6
juan-amorocho <jamoroch@gmail.com>
2019-07-21 17:59:21 +0200

    add basic submit form

--- spikes/how_to_test/Readme.md
+++ spikes/how_to_test/Readme.md
@@ -1,6 +1,9 @@
-# How to test a A/B Test tool using a spike
+# Spike to find out how to test a A/B Test framework
 ## Installing and all that jazz
 _Python3 is needed._
+
 `python3 -m venv .`
+
 `pip install -r requirements.txt`
-`flask run`
+
+`python -m flask run`
--- spikes/how_to_test/app/routes.py
+++ spikes/how_to_test/app/routes.py
@@ -1,6 +1,17 @@
+from flask import render_template, flash, redirect, url_for
 from app import app
-
+from app.forms import LoginForm
 @app.route('/')
 @app.route('/index')
 def index():
-  return "Hello, world!"
+  user = {'username':'Juan'}
+  return render_template('index.html', user = user)
+
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+  form = LoginForm()
+  if form.validate_on_submit():
+    flash('Login request for user {}'.format(form.username.data))
+    return redirect(url_for('index'))
+  return render_template('login.html', form = form)
