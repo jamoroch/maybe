39bb2f90d0225c8002e148dc3930b6196b190041
juan-amorocho <jamoroch@gmail.com>
2019-07-21 21:15:37 +0200

    basic scrapper that fills the SUT form

--- spikes/how_to_test/scraper/src/scraper.js
+++ spikes/how_to_test/scraper/src/scraper.js
@@ -1,10 +1,15 @@
-const rp = require('request-promise');
-const url = 'http://127.0.0.1:5000/index';
+const puppeteer = require('puppeteer');
 
-rp(url).then( function(html) {
+const url = 'http://127.0.0.1:5000/login';
+
+(async () => {
+  const launch_options = {headless: true};
+  const browser = await puppeteer.launch(launch_options);
+  const page = await browser.newPage();
+  await page.goto(url, {waitUntil: 'load'});
+  await page.type('#username', 'foo');
+  await page.click('#submit')
+  const html = await  page.content();
   console.log(html);
-})
-.catch(function(error) {
-  console.error(error);
-});
-
+  await browser.close();
+})();
